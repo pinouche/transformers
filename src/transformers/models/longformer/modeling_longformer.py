7b3b56832b8d360ddfc54d5e16bce812901dbaae
Thomas <26279201+pinouche@users.noreply.github.com>
2022-09-03 16:08:53 +0200

    Update modeling_longformer.py

--- src/transformers/models/longformer/modeling_longformer.py
+++ src/transformers/models/longformer/modeling_longformer.py
@@ -1289,7 +1289,7 @@ class LongformerEncoder(nn.Module):
         super().__init__()
         self.config = config
         self.layer = nn.ModuleList([LongformerLayer(config, layer_id=i) for i in range(config.num_hidden_layers)])
-        self.gradient_checkpointing = True
+        self.gradient_checkpointing = False
 
     def forward(
         self,
